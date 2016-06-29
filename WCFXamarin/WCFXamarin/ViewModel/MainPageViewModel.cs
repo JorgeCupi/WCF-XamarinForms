9b8fc72b084a984c347954078386dda743a454d3
Jorge Cupi <jocupi@microsoft.com>
2016-06-29 00:22:36 -0500

    First commit, uploading the whole project, ready to build and try

---
+++ WCFXamarin/WCFXamarin/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCFXamarin.Model;
+
+namespace WCFXamarin.ViewModel
+{
+    class MainPageViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        private Result _result;
+        public Result result
+        {
+            get { return _result; }
+            set
+            {
+                _result = value;
+                OnPropertyChanged("result");
+            }
+        }
+
+
+        private string _myVar;
+        public string myProp
+        {
+            get { return _myVar; }
+            set
+            {
+                _myVar = value;
+                OnPropertyChanged("myProp");
+            }
+        }
+
+        public MainPageViewModel()
+        {
+            result = new Result();
+            callWCF();
+        }
+
+        private void callWCF()
+        {
+            var wcfModel = new ServiceReference1.Service1Client();
+            var wcfViewModel = new ServiceReference1.Service1Client();
+
+            wcfModel.GetDataCompleted += WcfModel_GetDataCompleted; // Receive feedback asynchronously
+            wcfViewModel.GetDataCompleted += WcfViewModel_GetDataCompleted;
+
+            wcfModel.GetDataAsync("Model"); // Send request asynchronously
+            wcfViewModel.GetDataAsync("ViewModel");
+        }
+
+        private void WcfViewModel_GetDataCompleted(object sender, ServiceReference1.GetDataCompletedEventArgs e)
+        {
+            myProp = e.Result;
+        }
+
+        private void WcfModel_GetDataCompleted(object sender, ServiceReference1.GetDataCompletedEventArgs e)
+        {
+            result.message = e.Result;
+        }
+    }
+}
